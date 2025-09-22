9296f7a5b7ccedd9328b551a569ce400d7c17087
its-jade <jadele15@yahoo.com>
2025-09-22 16:50:43 -0400

    Refactor index.html: replace list items with cards, simplify script, and update column IDs

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,18 @@
+var StartList = document.getElementById("start-list");
+var ProgressList = document.getElementById("progress-list");
+var CompleteList = document.getElementById("completed-list");
 
-var StartList = document.getElementById('StartList');
-var ProgressList = document.getElementById('ProgressList');
-var CompleteList = document.getElementById('CompleteList');
-
- new Sortable(StartList, {
-    group: 'shared', // set both lists to same group
-    animation: 150
+new Sortable(StartList, {
+  group: "shared", // set both lists to same group
+  animation: 150,
 });
 
- new Sortable(ProgressList, {
-    group: 'shared', // set both lists to same group
-    animation: 150
+new Sortable(ProgressList, {
+  group: "shared", // set both lists to same group
+  animation: 150,
 });
 
- new Sortable(CompleteList, {
-    group: 'shared', // set both lists to same group
-    animation: 150
+new Sortable(CompleteList, {
+  group: "shared", // set both lists to same group
+  animation: 150,
 });
